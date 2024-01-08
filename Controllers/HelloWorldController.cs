b434734a56fe6cba594c1505d506460a71356dea
Vocal <vocalnight@protonmail.com>
2024-01-07 21:41:28 -0300

    Started tutorial

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MovieMvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //Get: /Helloworld/
+        public string Index()
+        {
+            return "Default";
+        }
+
+        // Get: /helloworld/welcome
+        public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }
+    }
+}
